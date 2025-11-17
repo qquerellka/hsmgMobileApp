2eaaf7437759f3901f2f0b1052d2090b48ebdce8
qquerellka <uskovmaksof@gmail.com>
2025-11-17 23:49:58 +0300

    fix MenuOverlay close

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Slot } from "expo-router";
+import React, { useEffect, useState } from "react";
+import { Slot, useSegments } from "expo-router";
 import { View, StatusBar } from "react-native";
 import { theme } from "@shared/config/theme";
 import { Screen } from "@shared/ui/Screen";
@@ -9,9 +9,13 @@ import { MenuOverlay } from "@/widgets/menu/MenuOverlay";
 export default function AppLayout() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  const handleToggleMenu = () => setIsMenuOpen(prev => !prev);
+  const handleToggleMenu = () => setIsMenuOpen((prev) => !prev);
   const handleCloseMenu = () => setIsMenuOpen(false);
+  const segments = useSegments();
 
+  useEffect(() => {
+    setIsMenuOpen(false);
+  }, [segments]);
   return (
     <View style={{ flex: 1, backgroundColor: theme.palette.totalBlack }}>
       <StatusBar
